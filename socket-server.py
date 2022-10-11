013c2b567403d6c19320cb24005195f1b8229487
12novel30 <79260472+12novel30@users.noreply.github.com>
2022-10-11 11:34:30 +0900

    22-10-11 in lecture, edit

--- socket-server.py
+++ socket-server.py
@@ -2,27 +2,31 @@ import socket
 
 
 def server_program():
-    # get the hostname
+    # 1st. get the hostname
     host = socket.gethostname()
     port = 5011  # initiate port no above 1024
 
+    # 2. create socket
     server_socket = socket.socket()  # get instance
     # look closely. The bind() function takes tuple as argument
+    # 3. bind socket
     server_socket.bind((host, port))  # bind host address and port together
 
+    # 4. listen socket
     # configure how many client the server can listen simultaneously
     server_socket.listen(2)
-    conn, address = server_socket.accept()  # accept new connection
+    # 5. accept socket -> wait
+    conn, address = server_socket.accept()  # accept new connection - return 2 things
     print("Connection from: " + str(address))
     while True:
-        # receive data stream. it won't accept data packet greater than 1024 bytes
-        data = conn.recv(1024).decode()
+        # receive data stream. it won't accept data packet greater than 1024 bytes -> decoding
+        data = conn.recv(1024).decode() # 1024: port number
         if not data:
             # if data is not received break
             break
         print("from connected user: " + str(data))
         data = input(' -> ')
-        conn.send(data.encode())  # send data to the client
+        conn.send(data.encode())  # send data to the client -> encoding
 
     conn.close()  # close the connection
 
